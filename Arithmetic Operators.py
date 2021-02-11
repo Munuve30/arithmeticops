70fb6cb3f6c60b2f219f86c7dc052c3e82e13f15
Munuve30 <77729985+Munuve30@users.noreply.github.com>
2021-02-11 11:01:35 +0300

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,34 @@
+#Arithmetic(+,-,/,//,*,%) with  integers
+#function of / & //
+a = 34
+c = 8
+print(a/c)
+print(a//c)
+# / - includes decimal numbers
+# // - doesnot include decimal numbers
+
+#function of +, -, * & %
+a = 45
+b = 34
+print(a+b)
+print(b-a)
+print(a-b)
+print(a*b)
+print(a%b)
+
+#Arithmetic(+,-,/,//,*,%) with  strings
+#only + operatore is possible with strings. - , / , , // , * , % aren`t possible
+x = "Joshua"
+y = "Musyimi"
+print(x+y)
+
+#Arithmetic(+,-,/,//,*,%) with  strings
+#all arithmetic operators possible
+t = 40.4
+s = 1.2
+print(t+s)
+print(t-s)
+print(t*s)
+print(t%s)
+print(t/s)
+
